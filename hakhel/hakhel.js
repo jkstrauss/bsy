00e3c193389f4d57d87c599bb6f163333a3ba5aa
Joseph K. Strauss <joseph.k.strauss@gmail.com>
2022-07-03 14:21:29 -0400

    Add intelinear layout

--- hakhel/hakhel.js
+++ hakhel/hakhel.js
@@ -99,6 +99,14 @@ app.controller('myCtrl', function($scope) {
 		bayith: (column) => column,
 		language: () => 'hebrew',
 		last: (column) => column == 3
+	}, {
+		name: 'interlinear',
+		languageEqualSize: true,
+		display: 'Interlinear',
+		lines: [0, 1],
+		bayith: (column, line) => column,
+		language: (column, line) => line == 0 ? 'hebrew' : 'english',
+		last: (column) => false
 	}
   ]
   $scope.displayOption = $scope.displayOptions[0]
